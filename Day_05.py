b5af98ab79feb7d71fa19c54fc5ed966f0997a02
Scott <scott@linfords.com>
2022-08-17 02:44:22 -0700

    Day 05 - code clean-up

--- Day_05.py
+++ Day_05.py
@@ -25,21 +25,20 @@ def part_two(door_id):
             index = int(hash[5], 16)
             c = hash[6]
             if index >= 8:
-                continue # ignore invalid positions
+                continue  # ignore invalid positions
             if password[index] != '-':
-                continue # ignore positions that have been filled in already
+                continue  # ignore positions that have been filled in already
             password[index] = c
             print(door_id, counter, hash, password)
     return ''.join(password)
 
-
-print(f'The password is: {part_two("cxdnnyjw")}')
 
 class Test(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual('18f47a30', part_one('abc'))
         self.assertEqual('f77a0e6e', part_one('cxdnnyjw'))
         pass
+
     def test_part_two(self):
         self.assertEqual('05ace8e3', part_two('abc'))
         self.assertEqual('999828ec', part_two('cxdnnyjw'))
