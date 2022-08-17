d2e9129c75f62d6712ffe7a6d6bf09a72cd6f9dc
Scott <scott@linfords.com>
2022-08-17 03:53:35 -0700

    Day 06 - part two passes

--- Day_06.py
+++ Day_06.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 
@@ -29,6 +30,17 @@ def pick_most_common(char_tallies: dict):
     return max_letter
 
 
+def pick_least_common(char_tallies):
+    min_value = math.inf
+    min_letter = None
+    for k, v in char_tallies.items():
+        if v < min_value:
+            min_value = v
+            min_letter = k
+
+    return min_letter
+
+
 def part_one(filename):
     data = load_puzzle_input(filename)
     width = len(data[0])
@@ -40,10 +52,22 @@ def part_one(filename):
     return ''.join(decrypted_message)
 
 
-print(part_one('Day_06_input.txt'))
+def part_two(filename):
+    data = load_puzzle_input(filename)
+    width = len(data[0])
+    decrypted_message = []
+    for i in range(width):
+        char_tallies = tally_chars(data, i)
+        c = pick_least_common(char_tallies)
+        decrypted_message.append(c)
+    return ''.join(decrypted_message)
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual('easter', part_one('Day_06_short_input.txt'))
         self.assertEqual('xhnqpqql', part_one('Day_06_input.txt'))
+
+    def test_part_two(self):
+        self.assertEqual('advent', part_two('Day_06_short_input.txt'))
+        self.assertEqual('brhailro', part_two('Day_06_input.txt'))
