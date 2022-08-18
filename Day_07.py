91c97aa74b1d886d40cb74ccac463c81a960b9e9
Scott <scott@linfords.com>
2022-08-18 00:58:46 -0700

    Day 07 - part two passes

--- Day_07.py
+++ Day_07.py
@@ -14,13 +14,13 @@ def split_words(x):
 
     for c in x:
         if c == '[':
-            assert(in_disallowed == False)
+            assert (in_disallowed == False)
             abba_allowed.append(''.join(current_word))
             current_word = []
             in_disallowed = True
             continue
         if c == ']':
-            assert(in_disallowed == True)
+            assert (in_disallowed == True)
             abba_disallowed.append(''.join(current_word))
             current_word = []
             in_disallowed = False
@@ -47,12 +47,22 @@ def has_abba(word):
     return False
 
 
+def find_abas(word):
+    abas = []
+    for i in range(len(word) - 2):
+        a = word[i]
+        b = word[i + 1]
+        c = word[i + 2]
+        if a == c and a != b:
+            abas.append(a + b + c)
+    return abas
 
-def is_valid(abba_allowed, abba_disallowed):
+
+def supports_tls(abba_allowed, abba_disallowed):
     abba_count = 0
     for x in abba_allowed:
         if has_abba(x):
-            abba_count +=1
+            abba_count += 1
     if abba_count == 0:
         return False
     for x in abba_disallowed:
@@ -61,16 +71,44 @@ def is_valid(abba_allowed, abba_disallowed):
     return True
 
 
+def supports_ssl(abba_allowed, abba_disallowed):
+    abas_outside = []
+    for x in abba_allowed:
+        abas_outside.extend(find_abas(x))
+    abas_inside = []
+    for x in abba_disallowed:
+        abas_inside.extend(find_abas(x))
+    for x in abas_outside:
+        a = x[0]
+        b = x[1]
+        bab = b + a + b
+        if bab in abas_inside:
+            return True
+    return False
+
 
 def part_one(filename):
     data = read_puzzle_input(filename)
+    valid_tally = 0
     for x in data:
         abba_allowed, abba_disallowed = split_words(x)
-        if is_valid(abba_allowed, abba_disallowed):
-            pass
+        if supports_tls(abba_allowed, abba_disallowed):
+            valid_tally += 1
+    return valid_tally
 
 
-part_one('Day_07_short_input.txt')
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    valid_tally = 0
+    for x in data:
+        abba_allowed, abba_disallowed = split_words(x)
+        if supports_ssl(abba_allowed, abba_disallowed):
+            valid_tally += 1
+    return valid_tally
+
+
+print('Supports SSL:', part_two('Day_07_input.txt'))
+
 
 class Test(unittest.TestCase):
     def test_split_words(self):
@@ -86,4 +124,10 @@ class Test(unittest.TestCase):
         self.assertTrue(has_abba('abba'))
         self.assertFalse(has_abba('asdfgh'))
 
+    def test_part_one(self):
+        self.assertEqual(110, part_one('Day_07_input.txt'))
+        self.assertEqual(2, part_one('Day_07_short_input.txt'))
 
+    def test_part_two(self):
+        self.assertEqual(3, part_two('Day_07_short_input_02.txt'))
+        self.assertEqual(242, part_two('Day_07_input.txt'))
