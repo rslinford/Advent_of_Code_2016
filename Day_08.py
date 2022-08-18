293a9cf743f1b0e01437c5e6d1d6b6b60d01ce3e
Scott <scott@linfords.com>
2022-08-18 13:56:58 -0700

    Day 08 - part one and two both pass

--- Day_08.py
+++ Day_08.py
@@ -26,11 +26,28 @@ class Screen:
                 self.grid[y][col] = self.grid[y-1][col]
             self.grid[0][col] = hold
 
+    def rotate_row(self, row, amount):
+        shift_size = amount % self.width
+        for _ in range(shift_size):
+            hold = self.grid[row][self.width-1]
+            for x in range(self.width - 1, 0, -1):
+                self.grid[row][x] = self.grid[row][x - 1]
+            self.grid[row][0] = hold
+
+    def count_lit_pixils(self):
+        tally = 0
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.grid[y][x] == '#':
+                    tally += 1
+        return tally
+
 
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
         data = f.read().strip().split('\n')
     return data
+
 
 def handle_rect_command(command, screen):
     result = re.search('^rect (\d+)x(\d+)$', command)
@@ -46,22 +63,86 @@ def handle_rotate_command(command, screen):
     amount = int(result.group(3))
     if x_or_y == 'x':
         screen.rotate_column(x_or_y_value, amount)
+    elif x_or_y == 'y':
+        screen.rotate_row(x_or_y_value, amount)
+    else:
+        raise ValueError(f'Expected "x" or "y" found "{x_or_y}"')
+
+def handle_command(command, screen):
+    if command.find('rect') == 0:
+        handle_rect_command(command, screen)
+    elif command.find('rotate') == 0:
+        handle_rotate_command(command, screen)
+    else:
+        raise ValueError(f'Expected "rect" or "rotate" found "{command}"')
 
 
 def part_one(filename):
-    data = read_puzzle_input(filename)
-    screen = Screen(3, 7)
-    for command in data:
-        if command.find('rect') == 0:
-            handle_rect_command(command, screen)
-        elif command.find('rotate') == 0:
-            handle_rotate_command(command, screen)
-        else:
-            raise ValueError(f'Expected "rect" or "rotate" found{command}')
+    commands = read_puzzle_input(filename)
+    screen = Screen(6, 50)
+    for command in commands:
+        handle_command(command, screen)
+    return screen.count_lit_pixils()
 
-part_one('Day_08_short_input.txt')
+
+def part_two(filename):
+    commands = read_puzzle_input(filename)
+    screen = Screen(6, 50)
+    for command in commands:
+        handle_command(command, screen)
+    print(screen)
+    return screen.count_lit_pixils()
+
+
+print('Lit pixel count:', part_two('Day_08_input.txt'))
 
 class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(121, part_one('Day_08_input.txt'))
+
+    def test_short_data(self):
+        commands = read_puzzle_input('Day_08_short_input.txt')
+        screen = Screen(3, 7)
+        self.assertEqual("[['.' '.' '.' '.' '.' '.' '.']\n"
+                         " ['.' '.' '.' '.' '.' '.' '.']\n"
+                         " ['.' '.' '.' '.' '.' '.' '.']]", str(screen))
+        index = 0
+        handle_command(commands[0], screen)
+        self.assertEqual("[['#' '#' '#' '.' '.' '.' '.']\n"
+                         " ['#' '#' '#' '.' '.' '.' '.']\n"
+                         " ['.' '.' '.' '.' '.' '.' '.']]", str(screen))
+        handle_command(commands[1], screen)
+        self.assertEqual("[['#' '.' '#' '.' '.' '.' '.']\n"
+                         " ['#' '#' '#' '.' '.' '.' '.']\n"
+                         " ['.' '#' '.' '.' '.' '.' '.']]", str(screen))
+        handle_command(commands[2], screen)
+        self.assertEqual("[['.' '.' '.' '.' '#' '.' '#']\n"
+                         " ['#' '#' '#' '.' '.' '.' '.']\n"
+                         " ['.' '#' '.' '.' '.' '.' '.']]", str(screen))
+        handle_command(commands[3], screen)
+        self.assertEqual("[['.' '#' '.' '.' '#' '.' '#']\n"
+                         " ['#' '.' '#' '.' '.' '.' '.']\n"
+                         " ['.' '#' '.' '.' '.' '.' '.']]", str(screen))
+        self.assertEqual(6, screen.count_lit_pixils())
+
+
+    def test_rotate_row(self):
+        screen = Screen(3, 4)
+        screen.grid[1][0] = '#'
+        screen.grid[1][3] = '#'
+        self.assertEqual("[['.' '.' '.' '.']\n"
+                         " ['#' '.' '.' '#']\n"
+                         " ['.' '.' '.' '.']]", str(screen))
+        screen.rotate_row(1, 1)
+        self.assertEqual("[['.' '.' '.' '.']\n"
+                         " ['#' '#' '.' '.']\n"
+                         " ['.' '.' '.' '.']]", str(screen))
+        screen.rotate_row(1, 2)
+        self.assertEqual("[['.' '.' '.' '.']\n"
+                         " ['.' '.' '#' '#']\n"
+                         " ['.' '.' '.' '.']]", str(screen))
+
+
     def test_rotate_column(self):
         screen = Screen(4, 4)
         screen.grid[0][2] = '#'
